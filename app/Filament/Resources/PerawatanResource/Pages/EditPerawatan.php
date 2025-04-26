cb15a64e5e6e1d918c7b51c2cddd709564a1c19c
khdfierror <ramadhannurkhadafi@gmail.com>
2025-04-26 20:46:54 +0800

    update: pemeliharaan

--- app/Filament/Resources/PerawatanResource/Pages/EditPerawatan.php
+++ app/Filament/Resources/PerawatanResource/Pages/EditPerawatan.php
@@ -15,7 +15,7 @@ class EditPerawatan extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 
